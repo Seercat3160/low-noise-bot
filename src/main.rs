fd63444d86e9a5751ae173215c60c52e8a61d02d
Seercat3160 <76834828+Seercat3160@users.noreply.github.com>
2024-10-26 12:23:27 +1100

    Clippy linting

--- src/main.rs
+++ src/main.rs
@@ -7,101 +7,6 @@ struct Data {} // stored and accessible in all command invocations
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
 
-/// Pin a message
-#[poise::command(context_menu_command = "Pin Message")]
-async fn pin_message(
-    ctx: Context<'_>,
-    #[description = "Message to pin"] message: serenity::Message,
-) -> Result<(), Error> {
-    println!(
-        "Asked to pin message {} by user {}",
-        message.id,
-        ctx.author().id
-    );
-
-    if !message.pinned {
-        match message.pin(ctx).await {
-            Ok(_) => {
-                println!(
-                    "Pinned message {} at request of user {}",
-                    message.id,
-                    ctx.author().id
-                );
-                ctx.send(
-                    CreateReply::default()
-                        .content(format!("📌 Pinned [message]({})", message.link())),
-                )
-                .await?;
-            }
-            Err(e) => {
-                println!("Failed to pin message {}: {}", message.id, e);
-                ctx.send(
-                    CreateReply::default()
-                        .ephemeral(true)
-                        .content(format!("📌 Failed to pin [message]({})!", message.link())),
-                )
-                .await?;
-            }
-        };
-    } else {
-        println!("Message {} is already pinned", message.id);
-        ctx.send(CreateReply::default().ephemeral(true).content(format!(
-            "📌 [That message]({}) is already pinned!",
-            message.link()
-        )))
-        .await?;
-    }
-
-    Ok(())
-}
-
-/// Unpin a message
-#[poise::command(context_menu_command = "Unpin Message")]
-async fn unpin_message(
-    ctx: Context<'_>,
-    #[description = "Message to unpin"] message: serenity::Message,
-) -> Result<(), Error> {
-    println!(
-        "Asked to unpin message {} by user {}",
-        message.id,
-        ctx.author().id
-    );
-
-    if message.pinned {
-        match message.unpin(ctx).await {
-            Ok(_) => {
-                println!(
-                    "Unpinned message {} at request of user {}",
-                    message.id,
-                    ctx.author().id
-                );
-                ctx.send(
-                    CreateReply::default()
-                        .content(format!("📌 Unpinned [message]({})", message.link())),
-                )
-                .await?;
-            }
-            Err(e) => {
-                println!("Failed to unpin message {}: {}", message.id, e);
-                ctx.send(
-                    CreateReply::default()
-                        .ephemeral(true)
-                        .content(format!("📌 Failed to unpin [message]({})!", message.link())),
-                )
-                .await?;
-            }
-        };
-    } else {
-        println!("Message {} is already not pinned", message.id);
-        ctx.send(CreateReply::default().ephemeral(true).content(format!(
-            "📌 [That message]({}) is not pinned!",
-            message.link()
-        )))
-        .await?;
-    }
-    Ok(())
-}
-
 #[tokio::main]
 async fn main() {
     println!("Discord bot is starting...");
@@ -142,4 +47,99 @@ async fn main() {
         .await;
     println!("Created client");
     client.unwrap().start().await.unwrap();
+}
+
+/// Pin a message
+#[poise::command(context_menu_command = "Pin Message")]
+async fn pin_message(
+    ctx: Context<'_>,
+    #[description = "Message to pin"] message: serenity::Message,
+) -> Result<(), Error> {
+    println!(
+        "Asked to pin message {} by user {}",
+        message.id,
+        ctx.author().id
+    );
+
+    if message.pinned {
+        println!("Message {} is already pinned", message.id);
+        ctx.send(CreateReply::default().ephemeral(true).content(format!(
+            "📌 [That message]({}) is already pinned!",
+            message.link()
+        )))
+        .await?;
+    } else {
+        match message.pin(ctx).await {
+            Ok(()) => {
+                println!(
+                    "Pinned message {} at request of user {}",
+                    message.id,
+                    ctx.author().id
+                );
+                ctx.send(
+                    CreateReply::default()
+                        .content(format!("📌 Pinned [message]({})", message.link())),
+                )
+                .await?;
+            }
+            Err(e) => {
+                println!("Failed to pin message {}: {}", message.id, e);
+                ctx.send(
+                    CreateReply::default()
+                        .ephemeral(true)
+                        .content(format!("📌 Failed to pin [message]({})!", message.link())),
+                )
+                .await?;
+            }
+        };
+    }
+
+    Ok(())
+}
+
+/// Unpin a message
+#[poise::command(context_menu_command = "Unpin Message")]
+async fn unpin_message(
+    ctx: Context<'_>,
+    #[description = "Message to unpin"] message: serenity::Message,
+) -> Result<(), Error> {
+    println!(
+        "Asked to unpin message {} by user {}",
+        message.id,
+        ctx.author().id
+    );
+
+    if message.pinned {
+        match message.unpin(ctx).await {
+            Ok(()) => {
+                println!(
+                    "Unpinned message {} at request of user {}",
+                    message.id,
+                    ctx.author().id
+                );
+                ctx.send(
+                    CreateReply::default()
+                        .content(format!("📌 Unpinned [message]({})", message.link())),
+                )
+                .await?;
+            }
+            Err(e) => {
+                println!("Failed to unpin message {}: {}", message.id, e);
+                ctx.send(
+                    CreateReply::default()
+                        .ephemeral(true)
+                        .content(format!("📌 Failed to unpin [message]({})!", message.link())),
+                )
+                .await?;
+            }
+        };
+    } else {
+        println!("Message {} is already not pinned", message.id);
+        ctx.send(CreateReply::default().ephemeral(true).content(format!(
+            "📌 [That message]({}) is not pinned!",
+            message.link()
+        )))
+        .await?;
+    }
+    Ok(())
 }
